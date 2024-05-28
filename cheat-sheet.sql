d1b1e6b17e75da11eebcef978f2c1ddf7356927a
aobarbieri <aobarbieri@gmail.com>
2024-05-28 11:22:32 -0400

    Create cheat sheet for SQL functions

---
+++ cheat-sheet.sql
@@ -0,0 +1,34 @@
+AVG()
+-- calculates de average.
+
+CEIL()
+-- rounds up to the next integer
+
+REPLACE(str, 'to be removed', 'to be added in place')
+-- removes a specific character and replace with another one or use '' to remove only.
+
+CAST(int AS CHAR)
+-- CAST is used to convert a value from one data type to another
+-- List of CAST specifications:
+-- SIGNED: Converts a value to a signed integer, which can include both positive and negative numbers.
+-- UNSIGNED: Converts a value to an unsigned integer, which can only include non-negative numbers.
+-- CHAR or CHARACTER: Converts a value to a character string.
+-- VARCHAR: Converts a value to a variable-length character string.
+-- BINARY: Converts a value to a binary string.
+-- VARBINARY: Converts a value to a variable-length binary string.
+-- TEXT: Converts a value to a text string.
+-- BLOB: Converts a value to a binary large object (BLOB).
+-- DATE: Converts a value to a date type, which includes only the date (year, month, and day).
+-- TIME: Converts a value to a time type, which includes only the time (hours, minutes, and seconds).
+-- DATETIME: Converts a value to a datetime type, which includes both the date and time.
+-- TIMESTAMP: Converts a value to a timestamp type, similar to DATETIME, often used for tracking record changes.
+-- YEAR: Converts a value to a year type.
+-- DECIMAL or NUMERIC: Converts a value to a fixed-point decimal, useful for precise financial calculations. Precision (total number of digits) and scale (number of digits after the decimal point) can be specified.
+-- FLOAT: Converts a value to a floating-point number, which can represent a wide range of values with fractional components.
+-- DOUBLE: Converts a value to a double-precision floating-point number, which offers more precision than FLOAT.
+-- REAL: Converts a value to a floating-point number, typically treated the same as FLOAT or DOUBLE depending on the database.
+-- BOOLEAN: Converts a value to a boolean type, representing TRUE or FALSE.
+-- JSON: Converts a value to a JSON type, which stores data in JSON (JavaScript Object Notation) format, useful for semi-structured data.
+
+
+
